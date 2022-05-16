4d686be246a14284e26c7142db8b25f22977f6cf
Tyler Bach <Tbach2@my.wctc.edu>
2022-05-15 22:08:11 -0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Color Picker',
+      theme: ThemeData.dark(),
+      home: const MyHomePage(title: 'Color Picker'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final Map<String, Color> colors = {
+    'purple': Colors.purple,
+    'red': Colors.red,
+    'blue': Colors.blue,
+    'orange': Colors.orange,
+  };
+
+  Color? selectedColor;
+
+  void _setColor(String colorName, Color color) {
+    setState(() {
+      selectedColor = color;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: selectedColor ?? Colors.black,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          for (var entry in colors.entries)
+            Container(
+              margin: EdgeInsets.all(10),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: entry.value,
+                  minimumSize: Size(300, 60)),
+                child: Text(''),
+                onPressed: () => _setColor(entry.key, entry.value),
+              ),
+            )
+        ],
+      ),
+    );
+  }
+}
